f4c9c06397b5253a80f818f99ee3d0ef30f6d122
Maria Bueno <125480470+mariaeduardabuenoda@users.noreply.github.com>
2026-05-15 18:51:44 -0300

    Atualizacao 15/05/2026
    Arrumei o Mapper

--- src/main/java/unicv/poo/eventos_api/mapper/LocalMapper.java
+++ src/main/java/unicv/poo/eventos_api/mapper/LocalMapper.java
@@ -2,6 +2,7 @@ package unicv.poo.eventos_api.mapper;
 
 import java.util.List;
 import org.mapstruct.Mapper;
+
 import unicv.poo.eventos_api.entity.Local;
 import unicv.poo.eventos_api.dto.LocalRequestDTO;
 import unicv.poo.eventos_api.dto.LocalResponseDTO;
