4b171db3be5f90af2def020e9f5a094368a9e144
Lucas Kolapouski <lkolapouski@gmail.com>
2026-05-16 14:10:11 -0300

    retira dependencia spring security

--- src/main/java/unicv/poo/eventos_api/repository/EventoRepository.java
+++ src/main/java/unicv/poo/eventos_api/repository/EventoRepository.java
@@ -3,7 +3,6 @@ package unicv.poo.eventos_api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import unicv.poo.eventos_api.entity.Evento;
-import unicv.poo.eventos_api.entity.Ingresso;
 
 @Repository
 public interface EventoRepository extends JpaRepository<Evento, Long> {
