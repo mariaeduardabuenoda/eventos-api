3f12002b435da18a84b310a85de92d8db489d1a2
Maria Bueno <125480470+mariaeduardabuenoda@users.noreply.github.com>
2026-05-19 19:52:22 -0300

    19/05/2026
    kkkkk

--- src/main/java/unicv/poo/eventos_api/repository/EventoRepository.java
+++ src/main/java/unicv/poo/eventos_api/repository/EventoRepository.java
@@ -2,12 +2,25 @@ package unicv.poo.eventos_api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+<<<<<<< Updated upstream
 import unicv.poo.eventos_api.entity.Evento;
+=======
+
+import unicv.poo.eventos_api.entity.Evento; // Ajustado para Evento
+>>>>>>> Stashed changes
 
 @Repository
 public interface EventoRepository extends JpaRepository<Evento, Long> {
 
+<<<<<<< Updated upstream
     // Ajustado para 'LocalId' e 'Capacidade', combinando com a sua Entity Evento
     boolean existsByLocalIdAndCapacidadeGreaterThan(Long localId, int capacidade);
     
 }
+=======
+    // O Spring Data JPA vai criar a lógica sozinho baseado neste nome
+    boolean existsByLocalIdAndParticipantesMaxGreaterThan(Long localId, int capacidade);
+    
+}
+
+>>>>>>> Stashed changes
